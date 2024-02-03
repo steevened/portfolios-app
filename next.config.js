8599d858a06d335090b3dd9ea455b8b505c567fd
steevened <alvaradosteven315@gmail.com>
2024-02-03 13:57:37 -0500

    configure github workflow

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     domains: ["localhost", "avatars.githubusercontent.com"],
   },
 };
 
-// module.exports = nextConfig;
-export default nextConfig;
+module.exports = nextConfig;
