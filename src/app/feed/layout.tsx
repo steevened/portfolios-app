939c3b544a737762086fc09f61217dbed7c075ac
steevened <alvaradosteven315@gmail.com>
2024-02-03 01:03:33 -0500

    fix params on feed layout

--- src/app/feed/layout.tsx
+++ src/app/feed/layout.tsx
@@ -1,22 +1,7 @@
-import TechnologiesGroup from "@/components/technologies-group";
-
 export default async function Layout({
-  searchParams,
   children,
 }: {
-  searchParams: {
-    [key: string]: string;
-  };
   children: React.ReactNode;
 }) {
-  return (
-    <div className="space-y-2.5 max-sm:py-2.5">
-      <div className="max-sm:px-2.5">
-        <TechnologiesGroup searchParams={searchParams} />
-      </div>
-      <div className="min-h-screen mx-auto max-w-screen-sm flex flex-col">
-        {children}
-      </div>
-    </div>
-  );
+  return <main>{children}</main>;
 }
